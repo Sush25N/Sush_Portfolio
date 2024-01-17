ea898c1caec2d03d5fa47853df5ce26011efda53
Sush25N <sushgabhane123@gmail.com>
2024-01-17 23:41:33 +0530

    navbar completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import { link } from "../data";
+import { links } from "../data";
 
 const Navbar = () => {
   return (
     <nav className="bg-emerald-200">
-      <div className="mx-auto max-w-7xl px-8 py-4 flex"></div>
+      <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-1 sm:items-center sm:py-8">
+        <h2 className="text-3xl font-bold">
+          Front-End <span className="text-emerald-600">Developer</span>
+        </h2>
+        <div className="flex gap-x-3">
+          {links.map((link) => {
+            const { id, href, text } = link;
+            return (
+              <a
+                key={id}
+                href={href}
+                className="capitalize text-lg tracking-wide hover:text-emerald-600 duration-300"
+              >
+                {text}
+              </a>
+            );
+          })}
+        </div>
+      </div>
     </nav>
   );
 };
