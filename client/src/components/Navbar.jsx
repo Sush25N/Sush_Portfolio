646f145362d76f30404d6456ba7328fc32bc5d2d
Sush25N <sushgabhane123@gmail.com>
2024-01-17 23:22:49 +0530

    importing navigations

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,11 @@
+import { link } from "../data";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-emerald-200">
+      <div className="mx-auto max-w-7xl px-8 py-4 flex"></div>
+    </nav>
+  );
 };
 
 export default Navbar;
